5f24996f6788e95b8fca7990ccdcc73bcf74ebe4
Zano <zanopcwr@protonmail.com>
2022-09-16 15:22:28 -0300

    TODO: fix directory iteration converion from path to string

--- main.cpp
+++ main.cpp
@@ -1,9 +1,56 @@
 #include <iostream>
 #include ".\lib\json.hpp"
+#include <fstream>
+#include <filesystem>
+#include <vector>
 
 using json = nlohmann::json;
 using namespace std;
+namespace fs = filesystem;
+
+struct processor {
+    int package = 0;
+
+};
+
+struct matrix {
+    
+};
+
+json readJson(string jsonDir) {
+    ifstream j(jsonDir);
+    return json::parse(j);
+}
+
+vector<string> getFileList(string path) {
+
+}
+
 
 int main() {
-    cout << "Testa" << endl;
+
+    //Define Configuration for tasks
+    json taskConfig = readJson("Test1.json");
+    //json applications = readJson(".\\Applications\\appA.json");
+    
+    vector<json> applications;
+    string path = ".\\Applications\\";
+    for (const auto & entry : fs::directory_iterator(path)) {
+        std::cout << entry.path() << std::endl;
+        string dir =  entry.path();
+        json app = readJson(dir);
+        applications.push(app);
+    }
+
+    ///This is useless
+    string _matrixSizeX = taskConfig["MPSOC_SIZE_X"];
+    string _matrixSizeY = taskConfig["MPSOC_SIZE_Y"];
+    string _tasksPerProcessor = taskConfig["TASKS_PER_PROCESSOR"];
+    //string _taskList = taskConfig["TEST"];
+
+    /// This is what is gonna be used
+    int matrixSizeX = atoi(_matrixSizeX.c_str());
+    int matrixSizeY = atoi(_matrixSizeY.c_str());
+    int tasksPerProcessor = atoi(_tasksPerProcessor.c_str());
+ 
 }
